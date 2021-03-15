62fce46db9bc020cdc821059efdf7d30a34e1935
Vadim Gush <vadimgush@protonmail.com>
2021-03-16 00:56:04 +0400

    Added more comments to queue.h file

--- structures/queue.h
+++ structures/queue.h
@@ -1,22 +1,44 @@
-/**
- * LIFO queue (stack), FIFO queue and deque
- */
 #pragma once
+
 #include "vector.h"
 
 /**
- * LIFO queue
+ * LIFO queue (also known as stack)
  */
 struct stack {
   struct vector data;
 };
 
+/**
+ * Creates an empty stack
+ *
+ * @returns empty stack
+ */
 struct stack stack_create();
 
-void stack_push(struct stack*, int value);
+/**
+ * Inserts a new value to the end (bottom) of the stack
+ * Complexity: O(1)
+ *
+ * @param stack - stack to modify
+ * @param value - value to insert
+ */
+void stack_push(struct stack* stack, int value);
 
+/**
+ * Reads and removes value from the end (bottom) of the stack
+ * Complexity: O(1)
+ *
+ * @param stack - stack to read and remove value from
+ * @returns a value
+ */
 int stack_pop(struct stack*);
 
+/**
+ * Returns a size of the stack
+ *
+ * @returns size of the stack
+ */
 size_t stack_size(struct stack*);
 
 /**
@@ -29,27 +51,36 @@ struct queue {
   size_t tail;
 };
 
+/**
+ * Creates a new empty queue
+ *
+ * @returns a new queue
+ */
 struct queue queue_create();
 
-void queue_push_back(struct queue*, int value);
+/**
+ * Inserts value at the end of the queue
+ * Complexity: O(1)
+ *
+ * @param queue - queue to insert value to
+ * @param value - value to insert
+ */
+void queue_push_back(struct queue* queue, int value);
 
 /**
- * Returns an element from the front of the queue
+ * Reads and removes value from the beginning of the queue 
+ * Complexity: O(1)
  *
- * Moves head further:
- *
- * [] [] [] [] [] []
- *  ^
- *  head
- *
- * After insert:
- *
- * [] [] [] [] [] []
- *     ^
- *     head
+ * @param queue - queue to remove value from
+ * @returns value from the beginning of the queue
  */
-int queue_pop_front(struct queue*);
+int queue_pop_front(struct queue* queue);
 
+/**
+ * Returns size of the queue
+ * 
+ * @returns size of the queue
+ */
 size_t queue_size(struct queue*);
 
 void queue_debug_print(struct queue*);

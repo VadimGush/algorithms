ed0c97783108080043146fac435ec04254685856
Vadim Gush <vadimgush@protonmail.com>
2021-12-20 23:49:37 +0400

    refactoring

---
+++ build/header.py
@@ -0,0 +1,51 @@
+#!/bin/python3
+# Python script to build one header for the static library.
+# Complete header file will be printer to stdout.
+# Run: ./header.py > build/include/gush.h
+import os
+import re
+
+header_names = []
+headers = []
+
+for subdir, dirs, files in os.walk("../src"):
+    for file in files:
+        if (file.endswith(".h") or file.endswith(".hpp")) and "experiments" not in subdir:
+            filepath = os.path.join(subdir, file)
+            headers.append(filepath)
+            header_names.append(file)
+
+local_header_pattern = re.compile(r"#include \".*\"")
+include_pattern = re.compile(r"#include <.*>")
+pragma_pattern = re.compile(r"#pragma once")
+
+content = []
+includes = set()
+for header in headers:
+    f = open(header)
+    lines = f.readlines()
+    for line in lines:
+        if local_header_pattern.match(line):
+            continue
+        if pragma_pattern.match(line):
+            continue
+        if include_pattern.match(line):
+            includes.add(line.replace("\n", ""))
+            continue
+        content.append(line)
+    f.close()
+
+license = open("LICENSE_INCLUDE")
+print("/*")
+for line in license.readlines():
+    print(line.replace("\n", ""))
+print("*/\n")
+license.close()
+
+print("#pragma once")
+for header in header_names:
+    print("// " + header)
+for include in includes:
+    print(include.replace("\n", ""))
+for line in content:
+    print(line.replace("\n", ""))
